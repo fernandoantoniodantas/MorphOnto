569651affa627531964d78a3a8b3557f6f45751a
Fernando Gomes Pinto <fpinto@inf.puc-rio.br>
2025-11-23 20:50:41 -0300

    Add script to initialize and activate conda environment

---
+++ Start_env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+set -e
+
+# Inicializa o conda nesse shell
+source /opt/miniconda3/etc/profile.d/conda.sh
+
+# Ativa o ambiente
+conda activate context_env
+
+echo "✔ Ambiente carregado!"
